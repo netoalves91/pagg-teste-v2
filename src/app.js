64feb3f270f9708ae4c2abc8802e58431c1216d9
neto <neeto_alves@hotmail.com>
2018-07-09 18:01:57 -0300

    Setup validations first form

--- src/app.js
+++ src/app.js
@@ -1,7 +1,55 @@
 import joao from './bootstrap.min.css';
 import styles from './stylesheet.sass';
 
-
-$(".xx").hide();
-
 console.log('webpack funcionando')
+
+$(".others-form").hide();
+
+
+$(".register-radio").click(function(){
+  var x = $(this).attr('value');
+
+  if(x == 'fisic'){
+    $(".pf-form").show();
+    $(".pj-form").hide();
+
+  }else if (x == 'juridic') {
+    $(".pj-form").show();
+    $(".pf-form").hide();
+  }
+});
+
+
+$(".register-input").blur(function(){
+  var value = $(this).attr('name')
+
+  if($(this).val() === ''){
+    if(!$(this).next().hasClass('error')){
+      $(this).after("<span class='error'> "+ value +" obrigatório</span>")
+    }
+  }else{
+    if($(this).next().hasClass('error')){
+      $(this).next().text('')
+    }
+  }
+
+  if($(this).hasClass('confirm-pass')){
+    var pass = $("#pass").val();
+    var passConfirm = $(this).val();
+
+    if (pass !== passConfirm){
+      if(!$(this).next().hasClass('error_pass')){
+        $(this).after("<span class='error_pass'>confirmação de senha diferente</span>")
+      }
+    }else {
+      $(this).next().text('')
+    }
+  }
+
+
+});
+
+
+
+
+// $("#xxx").inputmask("(99)[9]9999-9999", { placeholder: '' });
